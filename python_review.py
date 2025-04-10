c7a119444fcfbade992040482a6d8225ba208a10
Mariia Kovalenko <mariiakovalenko@MacBook-Pro-Mariia-2.local>
2025-04-10 09:55:47 -0400

    Levels complete

--- python_review.py
+++ python_review.py
@@ -1,1 +1,46 @@
-print("it worked!")
+# Name: Mariia Kovalenko
+
+#print("it worked!")
+
+#x = None
+
+#example = "I'm a string" # The variable called ecxample is now holding the string value "I'm a string"
+#a = 3 # the variable called a is holding a integer value 3
+#b = 4.0 #the variable called b and is holding a float value 4.0
+#c = True #the variable called c and is holding a boolean value True
+#d = False #the variable called d and is holding a boolean value False
+#e = "Hey" # The variable called e is now holding the string value "hey"
+#f = None # the variable is called f and its holding none value none
+#age = 32 # the variable is called age is holding integer value 32
+#instrument = "insert your instrument here"
+
+#'''
+#This is the first line of the comment
+#The second line
+#'''
+#prompt = "Enter your name: "
+#response = input(prompt)
+#print ("Hello", response)
+#print(response, "here's you agenda for the day...")
+
+#name = input ("What's your name ")
+#age = input 
+#print (name, age, type(name), type(age))
+
+#first_number = input ("What's the first number? ")
+#second_number = input ("What's the second number? ")
+#first_number = int(first_number)
+#second_number = int(second_number)
+#if first_number > second_number:
+#    print(first_number, "is bigger")
+#elif first_number == second_number:
+#    print("the numbers are equal")
+#else:
+#    print(second_number, "is bigger")
+
+#Problem5
+while " " in name:
+    name = name.replace(" ", "")
+name = input("Whats your full name? ")
+length_name = len(name)
+print(length_name)
